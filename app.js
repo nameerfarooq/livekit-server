081977d065062351f732a4631355f64b6dad6db5
muhammadNameer <nameerfarooquit@gmail.com>
2025-05-12 18:39:42 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// server.js
+import express from "express";
+import { AccessToken } from "livekit-server-sdk";
+import { config } from "dotenv";
+import cors from "cors";
+// const cors = require("cors");
+config();
+const createToken = async ({ participantName, isCreator }) => {
+  // If this room doesn't exist, it'll be automatically created when the first
+  // participant joins
+  const roomName = "quickstart-room";
+  // Identifier to be used for participant.
+  // It's available as LocalParticipant.identity with livekit-client SDK
+  //   const participantName = "quickstart-username";
+
+  const at = new AccessToken(
+    process.env.LIVEKIT_API_KEY,
+    process.env.LIVEKIT_API_SECRET,
+    {
+      identity: participantName,
+      // Token to expire after 10 minutes
+      ttl: "60m",
+    }
+  );
+  at.addGrant({
+    roomJoin: true,
+    room: roomName,
+    canPublish: isCreator ? true : false,
+    canPublishData: true,
+    // hidden: !isCreator ? true : false,
+  });
+  return await at.toJwt();
+};
+
+const app = express();
+const port = 3001;
+app.use(cors());
+app.use(express.json());
+
+app.post("/getToken", async (req, res) => {
+  const { participantName, isCreator } = req.body;
+  console.log("participantName : ", participantName);
+  console.log("isCreator : ", isCreator);
+  const token = await createToken(req.body);
+  res.json({ token });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
